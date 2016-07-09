ed69b507a71490acce32cf4099cf815ad0758d23
eclchoiz <eclchoiz@gmail.com>
2016-07-09 17:21:32 +0900

    testing DAO

--- app/src/main/java/com/choiz/apps/mileage/MileageDAO.java
+++ app/src/main/java/com/choiz/apps/mileage/MileageDAO.java
@@ -1,58 +1,23 @@
 package com.choiz.apps.mileage;
 
+import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
 /**
  * Created by SSENG on 2016-07-09.
  */
 public class MileageDAO {
+
+   /* private final static String DB_NAME = "mileage.db";
+    private final static String TABLE_NAME = "mil_table";
+    private final static String LOG_TAG = "MileageDAO";
+
+    SQLiteDatabase db;
+
+    private void createDataBase(String name) {
+        Log.d(LOG_TAG, "Creating DB [" + name + "].");
+
+        db = SQLiteDatabase.openOrCreateDatabase(name, MODE_WORLD_WRITEABLE,null);
+    }*/
 }
-
-/*
-
-public class dbOpenHelper extends SQLiteOpenHelper
-{
-    public static final String TABLE_NAME = "mileage";
-
-    public dbOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        createTable(db);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-    }
-    public void createTable(SQLiteDatabase db)
-    {
-        String sql = "CREATE TABLE " + TABLE_NAME + "(name text)";
-        try
-        {
-            db.execSQL(sql);
-        }
-        catch (SQLException e)
-        {
-        }
-    }
-
-    public void insertRecord(SQLiteDatabase db, MileageDTO dto)
-    {
-        db.beginTransaction();
-        try
-        {
-            String sql = "insert into " + TABLE_NAME + "(name)" + " values('" + name + "')";
-            db.execSQL(sql);
-            db.setTransactionSuccessful();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-        finally
-        {
-            db.endTransaction();
-        }
-    }
-}
-*/
